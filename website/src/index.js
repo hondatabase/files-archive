7f17f8faaef713db7c13a9410976e3695554a4c2
Flávio Pereira <github@flaviopereira.dev>
2024-11-13 14:19:22 +0000

    added api failsafe

--- website/src/index.js
+++ website/src/index.js
@@ -14,12 +14,12 @@ const router = createBrowserRouter([
     element: <FileExplorer />,
     loader: async ({ request }) => {
       const getContent = path => new Octokit().repos.getContent({ owner: 'hondatabase', repo: 'files-archive', path });
-      let response, files, metadata = {};
+      let response, files, metadata = {}, error = null;
 
       try {
         response = await getContent(new URL(request.url).pathname.slice(1) || '');
         files    = Array.isArray(response?.data) ? response.data : [response.data];
-      } catch { return { files: [], metadata: {}, loading: false }; }
+      } catch (e) { return { files: [], metadata: {}, loading: false, error: 'Failed to fetch content from GitHub' }; }
 
       const metadataFile = files.find(f => f.name === '.metadata.json');
       if (metadataFile) try {
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
 
       files = files.filter(f => !f.name.startsWith('.') && !hiddenItems.includes(f.name)); // Remove hidden and unnecessary files/folders
 
-      return { files, metadata, loading: false };
+      return { files, metadata, loading: false, error };
     }
   }
 ], {
