942f1310d5ad132f0e7f18b6a2ff0d6e118baa18
Flávio Pereira <github@flaviopereira.dev>
2024-12-28 00:36:48 +0000

    feat: gh link in statusbar

--- website/src/components/StatusBar.js
+++ website/src/components/StatusBar.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Github } from "lucide-react";
 
 export default ({fileCount, query, queryFileCount}) => (
 	<div className="fixed bottom-0 left-0 right-0 bg-gray-100 text-center p-2 border-t flex justify-between">
 		<div className="text-left">
-		{query ? `${queryFileCount} out of ${fileCount} for query "${query}".` : `${fileCount} file${fileCount === 1 ? "" : "s"}.`}
+			{query ? `${queryFileCount} out of ${fileCount} for query "${query}".` : `${fileCount} file${fileCount === 1 ? "" : "s"}.`}
+		</div>
+		<div className="text-right">
+			<a href="https://github.com/hondatabase/files-archive" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800 inline-flex items-center">
+				<Github className="mr-1 h-4 w-4" /> GitHub
+			</a>
 		</div>
 	</div>
 );
